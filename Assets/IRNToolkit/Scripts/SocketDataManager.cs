1bf5bf4b187d51b93c8d8174ec0720371ac6f5e1
ReiiYuki <voraton.l@ku.th>
2017-06-20 16:25:00 +0900

    Streaming Hand Position to Server

--- Assets/IRNToolkit/Scripts/SocketDataManager.cs
+++ Assets/IRNToolkit/Scripts/SocketDataManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using SocketIO;
-
+using Intel.RealSense.Hand;
 public class SocketDataManager : MonoBehaviour {
 
     SocketIOComponent socket;
@@ -11,6 +11,7 @@ public class SocketDataManager : MonoBehaviour {
 	void Awake () {
         
         socket = GetComponentInChildren<SocketIOComponent>();
+        GameObject.Find("DepthCameraManager").GetComponent<HandPositionManager>().AddSubscriber(gameObject);
         StartCoroutine(ConnectToServer());
 	}
 	
@@ -22,5 +23,28 @@ public class SocketDataManager : MonoBehaviour {
         data["type"] = "IR";
         socket.Emit("identify", new JSONObject(data));
         yield break;
+    }
+
+    void OnGesture(GestureData gesture)
+    {
+
+    }
+
+    void OnLeftHandChange(Vector3 handPosition)
+    {
+        Dictionary<string, string> data = new Dictionary<string, string>();
+        data["x"] = handPosition.x.ToString();
+        data["y"] = handPosition.y.ToString();
+        data["z"] = handPosition.z.ToString();
+        socket.Emit("updateLeftHandPosition", new JSONObject(data));
+    }
+
+    void OnRightHandChange(Vector3 handPosition)
+    {
+        Dictionary<string, string> data = new Dictionary<string, string>();
+        data["x"] = handPosition.x.ToString();
+        data["y"] = handPosition.y.ToString();
+        data["z"] = handPosition.z.ToString();
+        socket.Emit("updateRightHandPosition", new JSONObject(data));
     }
 }
