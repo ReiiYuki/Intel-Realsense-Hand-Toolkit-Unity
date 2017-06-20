9e093210357ea5a4a4403c59bf6df42ce0c5a6f3
ReiiYuki <voraton.l@ku.th>
2017-06-20 14:06:12 +0900

    Remove Debug Line

--- Assets/Example/Scripts/HandPositionHandler.cs
+++ Assets/Example/Scripts/HandPositionHandler.cs
@@ -13,13 +13,11 @@ public class HandPositionHandler : MonoBehaviour {
 
     void OnLeftHandChange(Vector3 handPosition)
     {
-        Debug.Log(handPosition);
         leftCursor.transform.position = Camera.main.transform.position + new Vector3(handPosition.x * 100, handPosition.y * 100, handPosition.z * 100) + Camera.main.transform.forward;
     }
 
     void OnRightHandChange(Vector3 handPosition)
     {
-        Debug.Log(handPosition);
         rightCursor.transform.position = Camera.main.transform.position + new Vector3(handPosition.x * 100, handPosition.y * 100, handPosition.z * 100) + Camera.main.transform.forward;
     }
 
