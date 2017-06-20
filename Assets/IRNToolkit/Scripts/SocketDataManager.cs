a3ce241f2898cf623a6c15ee9fd74c7fab56b4f9
ReiiYuki <voraton.l@ku.th>
2017-06-20 16:11:41 +0900

    Connect to Socket IO

---
+++ Assets/IRNToolkit/Scripts/SocketDataManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+
+public class SocketDataManager : MonoBehaviour {
+
+    SocketIOComponent socket;
+
+	// Use this for initialization
+	void Awake () {
+        
+        socket = GetComponentInChildren<SocketIOComponent>();
+        StartCoroutine(ConnectToServer());
+	}
+	
+    // Connect To Server
+	IEnumerator ConnectToServer()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Dictionary<string, string> data = new Dictionary<string, string>();
+        data["type"] = "IR";
+        socket.Emit("identify", new JSONObject(data));
+        yield break;
+    }
+}
